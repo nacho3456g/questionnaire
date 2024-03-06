7e73beecfbfe6d8be3095b82d85bda2098db42ae
nacho3456g <162060434+nacho3456g@users.noreply.github.com>
2024-03-06 02:19:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,67 +2,122 @@ let theContainer = document.getElementById(`container`);
 if (localStorage.getItem(`questionnaire`) !== null) {
     theContainer.style.display = `block`;
     theContainer.innerHTML = localStorage.getItem(`questionnaire`);
+    if (localStorage.getItem(`serial`) !== null) {
+        let serialNumber = document.getElementById(`serialNumber`);
+        serialNumber.innerHTML = localStorage.getItem(`serial`);
+    }
+    let serial = document.getElementById(`serial`);
+    serial.addEventListener(`blur`, function () {
+        if (serial.value.length === 3) {
+            localStorage.setItem(`serial`, serial.value);
+            serialNumber.innerHTML = localStorage.getItem(`serial`);
+        }
+    });
+    let allResults = [...document.querySelectorAll(`div.container div span`)];
+    let copyAll = allResults.reduce((accum, currEle) => {
+        return accum + `${currEle.innerHTML}\t`;
+    }, ``).trim();
+    let showResultDiv = document.getElementById(`showResult`);
+    let copyButton = document.getElementById(`copyButton`);
+    copyButton.addEventListener(`click`, function () {
+        if (/^\d{1}\t/.test(copyAll)) {
+            copyAll = localStorage.getItem(`serial`) + `\t` + copyAll;
+        }
+        if (/^\d{3}\t\d{1}/.test(copyAll)) {
+            copyAll = localStorage.getItem(`serial`) + copyAll.slice(3);
+        }
+        showResultDiv.style.display = `block`;
+        showResultDiv.innerHTML = `<pre>${copyAll}</pre>`;
+    });
 };
 let theGenerator = document.getElementById(`generator`);
 let newResultBox = document.getElementById(`newResult`);
-    theGenerator.addEventListener(`click`, function () {
-        newResultBox.style.display = `flex`;
-        setTimeout(_ => {
-            newResultBox.style.display = `none`;
-        }, 1000);
-        theContainer.style.display = `block`;
-        let a = 0;
-        let age = Math.round(Math.random() * 35) + 25;
-        let salary = Math.ceil(Math.random() * 3);
-        let wealth = Math.ceil(Math.random() * 2);
-        let uponChoice1 = Math.ceil(Math.random() * 2);
-        let uponChoice2 = Math.ceil(Math.random() * 2);
-        let recycle = Math.ceil(Math.random() * 2);
-        let wasteRemoval = Math.ceil(Math.random() * 4);
-        theContainer.innerHTML = `
-            <!--1--> <div>${++a} => 2</div>
-            <!--2--> <div>${++a} => ${age}</div>
-            <!--3--> <div>${++a} => ${Math.ceil(Math.random() * 2)}</div>
-            <!--4--> <div>${++a} => ${[1, 1, 1, 1, 1, 1, 2, 1, 1][Math.round(Math.random() * 8)]}</div>
-            <!--5--> ${age > 30 ? `<div>${++a} => ${[2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 2][Math.round(Math.random() * 10)]}</div>` : `<div>${++a} => ${[2, 2, 2, 2, 1, 1, 3, 2, 2, 2, 2][Math.round(Math.random() * 10)]}</div>`}
-            <!--6--> <div>${++a} => ${Math.ceil(Math.random() * 4)}</div>
-            <!--7--> <div>${++a} => ${Math.ceil(Math.random() * 4)}</div>
-            <!--8--> <div>${++a} => ${Math.ceil(Math.random() * 3)}</div>
-            <!--9--> <div>${++a} => ${salary}</div>
-            <!--10--> <div>${++a} => ${Math.ceil(Math.random() * 2)}</div>
-            <!--11--> <div>${++a} => 1</div>
-            <!--12--> <div>${++a} => 1</div>
-            <!--13--> <div>${++a} => 1</div>
-            <!--14--> <div>${++a} => ${recycle}</div>
-            <!--15--> <div>${++a} => ${wealth}</div>
-            <!--16--> ${recycle === 1 ? `<div>${++a} => 1</div>` : `<div>${++a} => 2</div>`}
-            <!--17--> <div>${++a} => ${Math.ceil(Math.random() * 2)}</div>
-            <!--18--> <div>${++a} => 1</div>
-            <!--19--> <div>${++a} => 1</div>
-            <!--20--> <div>${++a} => 1</div>
-            <!--21--> <div>${++a} => 1</div>
-            <!--22--> ${wealth === 1 ? `<div>${++a} => 1</div>` : `<div>${++a} => ${Math.ceil(Math.random() * 2) + 1}</div>`}
-            <!--23--> <div>${++a} => ${Math.ceil(Math.random() * 3)}</div>
-            <!--24--> <div>${++a} => 1</div>
-            <!--25--> <div>${++a} => 1</div>
-            <!--26--> <div>${++a} => 1</div>
-            <!--27--> <div>${++a} => ${Math.ceil(Math.random() * 3)}</div>
-            <!--28--> <div>${++a} => 1</div>
-            <!--29--> <div>${++a} => ${Math.ceil(Math.random() * 3)}</div>
-            <!--30--> <div>${++a} => ${salary === 3 ? Math.ceil(Math.random() * 2) : 2}</div>
-            <!--31--> <div>${++a} => ${[1, 4][Math.round(Math.random())]}</div>
-            <!--32--> <div>${++a} => ${Math.ceil(Math.random() * 2)}</div>
-            <!--33--> <div>${++a} => ${uponChoice1}</div>
-            <!--34--> ${uponChoice1 === 1 ? `<div>${++a} => ${[`plastics`, `paper`, `metals`, `cardboard`][Math.round(Math.random() * 3)]}</div>` : `<div>${++a} =></div>`}
-            <!--35--> <div>${++a} => ${uponChoice2}</div>
-            <!--36--> ${uponChoice2 === 1 ? `<div>${++a} => 1</div>` : `<div>${++a} =></div>`}
-            <!--37--> <div>${++a} => ${Math.ceil(Math.random() * 3)}</div>
-            <!--38--> <div>${++a} => ${Math.ceil(Math.random() * 2)}</div>
-            <!--39--> <div>${++a} => 1</div>
-            <!--40--> <div>${++a} => ${[15, 20, 25, 30][Math.round(Math.random() * 3)]}</div>
-            <!--41--> <div>${++a} => ${wasteRemoval}</div>
-            <!--42--> <div>${++a} => ${wasteRemoval === 1 ? Math.ceil(Math.random() * 2) : wasteRemoval === 2 ? Math.round(Math.random()) + 2 : Math.round(Math.random() * 2) + 3}</div>
+theGenerator.addEventListener(`click`, function () {
+    newResultBox.style.display = `flex`;
+    setTimeout(_ => {
+        newResultBox.style.display = `none`;
+    }, 1000);
+    theContainer.style.display = `block`;
+    let a = 0;
+    let age = Math.round(Math.random() * 35) + 25;
+    let salary = Math.ceil(Math.random() * 3);
+    let wealth = Math.ceil(Math.random() * 2);
+    let uponChoice1 = Math.ceil(Math.random() * 2);
+    let uponChoice2 = Math.ceil(Math.random() * 2);
+    let recycle = Math.ceil(Math.random() * 2);
+    let wasteRemoval = Math.ceil(Math.random() * 4);
+    theContainer.innerHTML = `
+            <div><div id="serialNumber">${localStorage.getItem(`serial`) || `Serial`}</div></div>
+            <div><p>If your serial Number is already inserted, you don't have to insert it again!</p></div>
+            <div class="copy-questionnaire" id="copy"><input type="number" id="serial" placeholder="Serial" title="If your serial Number is already inserted, you don't have to insert it again!"><div id="copyButton">Show Final Result</div></div>
+            <div id="showResult" contenteditable></div>
+            <!--1--> <div>${++a} => <span>2</span></div>
+            <!--2--> <div>${++a} => <span>${age}</span></div>
+            <!--3--> <div>${++a} => <span>${Math.ceil(Math.random() * 2)}</span></div>
+            <!--4--> <div>${++a} => <span>${[1, 1, 1, 1, 1, 1, 2, 1, 1][Math.round(Math.random() * 8)]}</span></div>
+            <!--5--> ${age > 30 ? `<div>${++a} => <span>${[2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 2][Math.round(Math.random() * 10)]}</span></div>` : `<div>${++a} => <span>${[2, 2, 2, 2, 1, 1, 3, 2, 2, 2, 2][Math.round(Math.random() * 10)]}</span></div>`}
+            <!--6--> <div>${++a} => <span>${Math.ceil(Math.random() * 4)}</span></div>
+            <!--7--> <div>${++a} => <span>${Math.ceil(Math.random() * 4)}</span></div>
+            <!--8--> <div>${++a} => <span>${Math.ceil(Math.random() * 3)}</span></div>
+            <!--9--> <div>${++a} => <span>${salary}</span></div>
+            <!--10--> <div>${++a} => <span>${Math.ceil(Math.random() * 2)}</span></div>
+            <!--11--> <div>${++a} => <span>1</span></div>
+            <!--12--> <div>${++a} => <span>1</span></div>
+            <!--13--> <div>${++a} => <span>1</span></div>
+            <!--14--> <div>${++a} => <span>${recycle}</span></div>
+            <!--15--> <div>${++a} => <span>${wealth}</span></div>
+            <!--16--> ${recycle === 1 ? `<div>${++a} => <span>1</span></div>` : `<div>${++a} => <span>2</span></div>`}
+            <!--17--> <div>${++a} => <span>${Math.ceil(Math.random() * 2)}</span></div>
+            <!--18--> <div>${++a} => <span>1</span></div>
+            <!--19--> <div>${++a} => <span>1</span></div>
+            <!--20--> <div>${++a} => <span>1</span></div>
+            <!--21--> <div>${++a} => <span>1</span></div>
+            <!--22--> ${wealth === 1 ? `<div>${++a} => <span>1</span></div>` : `<div>${++a} => <span>${Math.ceil(Math.random() * 2) + 1}</span></div>`}
+            <!--23--> <div>${++a} => <span>${Math.ceil(Math.random() * 3)}</span></div>
+            <!--24--> <div>${++a} => <span>1</span></div>
+            <!--25--> <div>${++a} => <span>1</span></div>
+            <!--26--> <div>${++a} => <span>1</span></div>
+            <!--27--> <div>${++a} => <span>${Math.ceil(Math.random() * 3)}</span></div>
+            <!--28--> <div>${++a} => <span>1</span></div>
+            <!--29--> <div>${++a} => <span>${Math.ceil(Math.random() * 3)}</span></div>
+            <!--30--> <div>${++a} => <span>${salary === 3 ? Math.ceil(Math.random() * 2) : 2}</span></div>
+            <!--31--> <div>${++a} => <span>${[1, 4][Math.round(Math.random())]}</span></div>
+            <!--32--> <div>${++a} => <span>${Math.ceil(Math.random() * 2)}</span></div>
+            <!--33--> <div>${++a} => <span>${uponChoice1}</span></div>
+            <!--34--> ${uponChoice1 === 1 ? `<div>${++a} => <span>${[`plastics`, `paper`, `metals`, `cardboard`][Math.round(Math.random() * 3)]}</span></div>` : `<div>${++a} => <span></span></div>`}
+            <!--35--> <div>${++a} => <span>${uponChoice2}</span></div>
+            <!--36--> ${uponChoice2 === 1 ? `<div>${++a} => <span>1</span></div>` : `<div>${++a} => <span></span></div>`}
+            <!--37--> <div>${++a} => <span>${Math.ceil(Math.random() * 3)}</span></div>
+            <!--38--> <div>${++a} => <span>${Math.ceil(Math.random() * 2)}</span></div>
+            <!--39--> <div>${++a} => <span>1</span></div>
+            <!--40--> <div>${++a} => <span>${[15, 20, 25, 30][Math.round(Math.random() * 3)]}</span></div>
+            <!--41--> <div>${++a} => <span>${wasteRemoval}</span></div>
+            <!--42--> <div>${++a} => <span>${wasteRemoval === 1 ? Math.ceil(Math.random() * 2) : wasteRemoval === 2 ? Math.round(Math.random()) + 2 : Math.round(Math.random() * 2) + 3}</span></div>
         `;
-        a = 0;
-        localStorage.setItem(`questionnaire`, theContainer.innerHTML);
-    });
+        let allResults = [...document.querySelectorAll(`div.container div span`)];
+        let copyAll = allResults.reduce((accum, currEle) => {
+            return accum + `${currEle.innerHTML}\t`;
+        }, ``).trim();
+        let serial = document.getElementById(`serial`);
+        let serialNumber = document.getElementById(`serialNumber`);
+        serial.addEventListener(`blur`, function () {
+            if (serial.value.length === 3) {
+                localStorage.setItem(`serial`, serial.value);
+                serialNumber.innerHTML = localStorage.getItem(`serial`);
+            }
+        });
+        let showResultDiv = document.getElementById(`showResult`);
+        let copyButton = document.getElementById(`copyButton`);
+        copyButton.addEventListener(`click`, function () {
+            if (/^\d{1}\t/.test(copyAll)) {
+                copyAll = localStorage.getItem(`serial`) + `\t` + copyAll;
+            }
+            if (/^\d{3}\t\d{1}/.test(copyAll)) {
+                copyAll = localStorage.getItem(`serial`) + copyAll.slice(3);
+            }
+            showResultDiv.style.display = `block`;
+            showResultDiv.innerHTML = `<pre>${copyAll}</pre>`;
+        });
+    a = 0;
+    localStorage.setItem(`questionnaire`, theContainer.innerHTML);
+});
